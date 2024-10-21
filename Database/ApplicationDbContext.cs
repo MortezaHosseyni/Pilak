7d8f8381db3bf2fea323b680a35313e378375fa8
Morteza Hosseini <morteza_hosseini_dev@outlook.com>
2024-10-21 14:45:21 +0330

    Database Install EFCore with Tools and SQLite packages Create ApplicationDbContext and inherited this from DbContext Create new migration and update database Ignore local database

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pilak.Database.Entities;
+
+namespace Pilak.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<License> Licenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var databasePath = Path.Combine(Directory.GetCurrentDirectory(), "Pilak.db");
+            optionsBuilder.UseSqlite($"Data Source={databasePath};");
+        }
+    }
+}
---
+++ Migrations/20241021111242_Init.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pilak.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 225, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 225, nullable: false),
+                    Address = table.Column<string>(type: "TEXT", maxLength: 800, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", maxLength: 11, nullable: false),
+                    Bio = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 800, nullable: true),
+                    Picture = table.Column<string>(type: "TEXT", maxLength: 225, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Licenses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirstDigitSection = table.Column<int>(type: "INTEGER", nullable: false),
+                    LetterSection = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    SecondDigitSection = table.Column<int>(type: "INTEGER", nullable: false),
+                    CityCode = table.Column<int>(type: "INTEGER", nullable: false),
+                    IssueDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ExpiryDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PersonId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Licenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Licenses_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Licenses_PersonId",
+                table: "Licenses",
+                column: "PersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Licenses");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
