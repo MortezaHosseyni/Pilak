e0c1bad793163d3549a467b94cf0f2044dd96871
Morteza Hosseini <morteza_hosseini_dev@outlook.com>
2024-10-22 19:34:25 +0330

    RealTime Detection (UI) Add Person information user control to this section Create camera detection section with content

--- formMain.Designer.cs
+++ formMain.Designer.cs
@@ -38,13 +38,25 @@
             btn_SelectPlateImage = new Button();
             img_PlateImage = new PictureBox();
             tbp_DetectRealtime = new TabPage();
+            pnl_RealTimeDetection = new Panel();
+            cmb_Cameras = new ComboBox();
+            rtb_RealTimeDetectionLog = new RichTextBox();
+            btn_RealTimeDetect = new Button();
+            img_Camera = new PictureBox();
+            pnl_rtTab_UserInformation = new Panel();
+            ucPersonInformation1 = new ucPersonInformation();
             tbp_RegisteredPlates = new TabPage();
             tbp_RegisteredPersons = new TabPage();
+            btn_ChooseCamera = new Button();
             tab_MainTab.SuspendLayout();
             tbp_DetectByPicture.SuspendLayout();
             pnl_UserInformation.SuspendLayout();
             pnl_PlateDetection.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)img_PlateImage).BeginInit();
+            tbp_DetectRealtime.SuspendLayout();
+            pnl_RealTimeDetection.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)img_Camera).BeginInit();
+            pnl_rtTab_UserInformation.SuspendLayout();
             SuspendLayout();
             // 
             // tab_MainTab
@@ -87,10 +99,10 @@
             // 
             usc_PersonInformation.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
             usc_PersonInformation.Font = new Font("IRANSansX", 9F);
-            usc_PersonInformation.Location = new Point(3, 1);
+            usc_PersonInformation.Location = new Point(3, 4);
             usc_PersonInformation.Margin = new Padding(3, 4, 3, 4);
             usc_PersonInformation.Name = "usc_PersonInformation";
-            usc_PersonInformation.Size = new Size(484, 647);
+            usc_PersonInformation.Size = new Size(484, 644);
             usc_PersonInformation.TabIndex = 0;
             // 
             // pnl_PlateDetection
@@ -139,14 +151,86 @@
             // 
             // tbp_DetectRealtime
             // 
-            tbp_DetectRealtime.Location = new Point(4, 24);
+            tbp_DetectRealtime.Controls.Add(pnl_RealTimeDetection);
+            tbp_DetectRealtime.Controls.Add(pnl_rtTab_UserInformation);
+            tbp_DetectRealtime.Location = new Point(4, 27);
             tbp_DetectRealtime.Margin = new Padding(3, 4, 3, 4);
             tbp_DetectRealtime.Name = "tbp_DetectRealtime";
             tbp_DetectRealtime.Padding = new Padding(3, 4, 3, 4);
-            tbp_DetectRealtime.Size = new Size(974, 675);
+            tbp_DetectRealtime.Size = new Size(974, 672);
             tbp_DetectRealtime.TabIndex = 1;
             tbp_DetectRealtime.Text = "شناسایی همزمان";
             tbp_DetectRealtime.UseVisualStyleBackColor = true;
+            // 
+            // pnl_RealTimeDetection
+            // 
+            pnl_RealTimeDetection.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            pnl_RealTimeDetection.Controls.Add(btn_ChooseCamera);
+            pnl_RealTimeDetection.Controls.Add(cmb_Cameras);
+            pnl_RealTimeDetection.Controls.Add(rtb_RealTimeDetectionLog);
+            pnl_RealTimeDetection.Controls.Add(btn_RealTimeDetect);
+            pnl_RealTimeDetection.Controls.Add(img_Camera);
+            pnl_RealTimeDetection.Location = new Point(6, 7);
+            pnl_RealTimeDetection.Name = "pnl_RealTimeDetection";
+            pnl_RealTimeDetection.Size = new Size(466, 652);
+            pnl_RealTimeDetection.TabIndex = 1;
+            // 
+            // cmb_Cameras
+            // 
+            cmb_Cameras.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            cmb_Cameras.FormattingEnabled = true;
+            cmb_Cameras.Location = new Point(3, 371);
+            cmb_Cameras.Name = "cmb_Cameras";
+            cmb_Cameras.Size = new Size(427, 26);
+            cmb_Cameras.TabIndex = 3;
+            // 
+            // rtb_RealTimeDetectionLog
+            // 
+            rtb_RealTimeDetectionLog.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            rtb_RealTimeDetectionLog.Location = new Point(3, 487);
+            rtb_RealTimeDetectionLog.Name = "rtb_RealTimeDetectionLog";
+            rtb_RealTimeDetectionLog.Size = new Size(460, 161);
+            rtb_RealTimeDetectionLog.TabIndex = 2;
+            rtb_RealTimeDetectionLog.Text = "";
+            // 
+            // btn_RealTimeDetect
+            // 
+            btn_RealTimeDetect.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btn_RealTimeDetect.Location = new Point(3, 402);
+            btn_RealTimeDetect.Name = "btn_RealTimeDetect";
+            btn_RealTimeDetect.Size = new Size(460, 43);
+            btn_RealTimeDetect.TabIndex = 1;
+            btn_RealTimeDetect.Text = "شناسایی";
+            btn_RealTimeDetect.UseVisualStyleBackColor = true;
+            // 
+            // img_Camera
+            // 
+            img_Camera.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            img_Camera.BorderStyle = BorderStyle.FixedSingle;
+            img_Camera.Location = new Point(3, 4);
+            img_Camera.Name = "img_Camera";
+            img_Camera.Size = new Size(460, 361);
+            img_Camera.TabIndex = 0;
+            img_Camera.TabStop = false;
+            // 
+            // pnl_rtTab_UserInformation
+            // 
+            pnl_rtTab_UserInformation.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            pnl_rtTab_UserInformation.Controls.Add(ucPersonInformation1);
+            pnl_rtTab_UserInformation.Location = new Point(478, 7);
+            pnl_rtTab_UserInformation.Name = "pnl_rtTab_UserInformation";
+            pnl_rtTab_UserInformation.Size = new Size(490, 652);
+            pnl_rtTab_UserInformation.TabIndex = 0;
+            // 
+            // ucPersonInformation1
+            // 
+            ucPersonInformation1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            ucPersonInformation1.Font = new Font("IRANSansX", 9F);
+            ucPersonInformation1.Location = new Point(3, 4);
+            ucPersonInformation1.Margin = new Padding(3, 4, 3, 4);
+            ucPersonInformation1.Name = "ucPersonInformation1";
+            ucPersonInformation1.Size = new Size(484, 644);
+            ucPersonInformation1.TabIndex = 0;
             // 
             // tbp_RegisteredPlates
             // 
@@ -168,6 +252,15 @@
             tbp_RegisteredPersons.Text = "شهروندان ثبت شده";
             tbp_RegisteredPersons.UseVisualStyleBackColor = true;
             // 
+            // btn_ChooseCamera
+            // 
+            btn_ChooseCamera.Location = new Point(433, 371);
+            btn_ChooseCamera.Name = "btn_ChooseCamera";
+            btn_ChooseCamera.Size = new Size(30, 26);
+            btn_ChooseCamera.TabIndex = 4;
+            btn_ChooseCamera.Text = "✅";
+            btn_ChooseCamera.UseVisualStyleBackColor = true;
+            // 
             // formMain
             // 
             AutoScaleDimensions = new SizeF(7F, 18F);
@@ -184,6 +277,10 @@
             pnl_UserInformation.ResumeLayout(false);
             pnl_PlateDetection.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)img_PlateImage).EndInit();
+            tbp_DetectRealtime.ResumeLayout(false);
+            pnl_RealTimeDetection.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)img_Camera).EndInit();
+            pnl_rtTab_UserInformation.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -200,5 +297,13 @@
         private RichTextBox rtb_PlateDetectionLog;
         private Panel pnl_UserInformation;
         private ucPersonInformation usc_PersonInformation;
+        private Panel pnl_rtTab_UserInformation;
+        private ucPersonInformation ucPersonInformation1;
+        private Panel pnl_RealTimeDetection;
+        private PictureBox img_Camera;
+        private Button btn_RealTimeDetect;
+        private RichTextBox rtb_RealTimeDetectionLog;
+        private ComboBox cmb_Cameras;
+        private Button btn_ChooseCamera;
     }
 }
